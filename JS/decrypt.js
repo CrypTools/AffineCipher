5491872dba4a664fdf0184eef7a80abb15aa6154
Arthur Guiot <arguiot@gmail.com>
2017-12-17 16:29:20 +0800

    Added JS files, but not working

--- JS/decrypt.js
+++ JS/decrypt.js
@@ -1,1 +1,13 @@
-to edit
+String.prototype.decrypt = function(key, a, b) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const keyArray = key.split("-")
+	let array = []
+	for (let i of this) {
+		array.push(alphabet.indexOf(i))
+	}
+	let output = "";
+	for (var i = 0; i < array.length; i++) {
+		const image = alphabet[(keyArray[i] * 26 + array[i] - b) / a]
+		output += image
+	}
+}
--- JS/encrypt.js
+++ JS/encrypt.js
@@ -1,1 +1,22 @@
-to edit
+String.prototype.encrypt = function(a, b) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+	let array = [];
+	for (let i of this) {
+		array.push(alphabet.indexOf(i.toLowerCase()))
+	}
+	let output = "";
+	let cle;
+	let divtem = "";
+	for (let i of array) {
+		const image = alphabet[i * a + b % 26]
+		output += image
+		const div = Math.floor(i * a + b / 26).toString()
+		if (i == 0) {
+			divtem = div
+		} else {
+			divem = "-" + div
+		}
+		cle += divtem
+	}
+	return [output, cle]
+}
