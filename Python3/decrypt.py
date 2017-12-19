0a427dea3a4e0cba887e44f44da8df817fbfa434
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2017-12-19 13:25:55 +1100

    Update decrypt.py

--- Python3/decrypt.py
+++ Python3/decrypt.py
@@ -1,17 +1,17 @@
 import string
 
-def decrypt():
-  initial = input('Give the message (no space or special characters) : ')
-  cle = input("Give the key : ")
+def decrypt(initial, cle, a, b):
+  
+  """  Use: decrypt("message", 45, 65)
+=> Encrypted message : hlrrnxl
+=> Key : 23-9-33-33-2-12-9
+"""
+
   quotients = cle.split("-")
   initial = initial.lower()
   numeros = []
   for character in initial:
       numeros.append(ord(character) - 97)
-  
-  a = int(input('Give the value of a : '))
-  b = int(input('Give the value of b : '))
-  
   output = ""
   alphabet = dict(zip(range(0, 26), string.ascii_lowercase))
 
@@ -19,4 +19,4 @@ def decrypt():
     image = str(alphabet[int(((int(quotients[i]) * 26 + numeros[i] - b) / a))])
     output += image
     
-  print("Decrypted message : " + output)
+  print(output)
